c88db409f64cf337de543201a643768b0441c2d9
Pham  Quoc Anh <phamquocanha5k02@gmail.com>
2026-04-23 09:53:26 +0700

    btvn và phân tích

---
+++ baitapvenha4.sql
@@ -0,0 +1,22 @@
+create database phone_management_db;
+
+use phone_management_db;
+
+create table USERS(
+	user_id int primary key auto_increment,
+    user_name varchar(255) not null,
+	phone int not null
+);
+
+alter table USERS
+add column phone_new varchar(15) null;
+
+update USERS
+set phone_new = concat('0, phone')
+where phone_new is null;
+
+alter table USERS
+drop column phone;
+
+alter table USERS
+change column phone_new phone varchar(15);
